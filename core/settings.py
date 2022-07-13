95c8a9ce7d302b4011e5e69dc6b3b77babb08084
Antharia <artenischloria@gmail.com>
2022-07-13 11:13:34 +0000

    FEAT environments variables

--- core/settings.py
+++ core/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-qh1(q=l%kg&g*=3!i-k%ll@_8@!5+xoi5uxbz33d*#0y8l9(aw'
+SECRET_KEY = env.str("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
